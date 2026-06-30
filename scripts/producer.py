2ad40999b90a8761fd0b5721276574cda3b8f720
Daniele De Martino <danieledemartino.72004@gmail.com>
2026-06-30 18:02:57 +0200

    Feat: added data extraction Python script
    The data is collected from the API and published through a Kafka producer.

---
+++ scripts/producer.py
@@ -0,0 +1,59 @@
+import time
+import requests
+import json
+from confluent_kafka import Producer
+
+config = {
+  'bootstrap.servers': 'localhost:9092',
+  'client.id': 'dotturin-bike-producer',
+  'message.max.bytes': 10485760
+}
+
+producer = Producer(config)
+
+API_URL = "https://gbfs.api.ridedott.com/public/v2/turin/free_bike_status.json"
+TOPIC_NAME = "dotturin-bike-status"
+
+def producer_callback(err, msg):
+  if err is not None:
+    print(f"[-] Delivery error: {err}")
+  else:
+    print(f"[+] Message sent to {msg.topic()}")
+
+def fetch_free_bikes(url):
+  response = requests.get(url)
+
+  if response.status_code == 200:
+    data = response.json()
+
+    return data
+  else:
+    print(f"[-] API error! Status code: {response.status_code}")
+
+    return None
+
+def main():
+  try:
+    print(f"\n[*] API request: {API_URL}")
+      
+    data = fetch_free_bikes(API_URL)
+      
+    if data is not None:
+      payload = json.dumps(data)
+        
+      producer.produce(
+        topic=TOPIC_NAME, 
+        value=payload.encode('utf-8'), 
+        callback=producer_callback
+      )
+        
+      # TEMPORARY for testing [TODO: remove]
+      producer.flush()
+   
+  except Exception as e:
+    print(f"[-] Runtime error: {e}")
+
+if __name__ == "__main__":
+    print("[*] Starting Kafka producer...")
+
+    main()
