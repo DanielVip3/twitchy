c7f4987ea45c15911c411132cac4dd6ea9a9259c
Daniele De Martino <danieledemartino.72004@gmail.com>
2026-07-05 19:22:24 +0200

    Fix: remove dangling component causing an error

--- dashboard/components/__init__.py
+++ dashboard/components/__init__.py
@@ -3,7 +3,6 @@ from .events_stats import events_stats
 from .kpis import kpis
 from .language_hour import language_hour
 from .sorted_hbar import sorted_hbar
-from .tags_vs_viewers import tags_vs_viewers
 from .tags import tags
 from .top_games_streamers import top_games_streamers
 from .viewer_trend import viewer_trend
